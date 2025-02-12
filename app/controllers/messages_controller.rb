c3d5923baf751a4e0002e29efd049d3171a122bd
Hou Yuanjie <houyuanjie555@qq.com>
2025-02-12 19:21:08 +0800

    Rails new

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,29 @@
+class MessagesController < ApplicationController
+  before_action :set_room, only: %i[ new create ]
+
+  def new
+    @message = @room.messages.new
+  end
+
+  def create
+    @message = @room.messages.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to @room }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
